1fb589287e48bf83f1e04cd7b15c16450ee775d3
yuninurafrila <yuninurafrila@gmail.com>
2026-03-31 20:00:12 +0700

    Revert "update"
    This reverts commit e7e51348837f6bc6d33e0b17e503fa37c76a5612.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,4 @@
 <h2>Sistem Keuangan Kos</h2>
 <p>Di masa kini yang sudah memasuki zaman digital, kemajuan teknologi membuat berbagai kegiatan menjadi lebih cepat, mudah, dan hemat waktu. Digitalisasi bukan lagi pilihan, melainkan kebutuhan—termasuk dalam pengelolaan bisnis kos.
 KostPay hadir sebagai solusi digital yang membantu pemilik kos dalam mengelola properti secara lebih modern dan teratur.</p>
-<p>Tugas latihan ci/cd</p>
 @endsection
